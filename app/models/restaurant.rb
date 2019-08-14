165dc4c4c4cccaf76013bee834d4f65fa35b1445
Faustine Fourdinier <lafaustinbox@gmail.com>
2019-08-14 15:38:49 +0200

    exo restaurants and reviews with no style

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+
   has_many :reviews, dependent: :destroy
 
   validates :name, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating fake restaurants and reviews...'
+
+5.times do
+  restaurant_seed = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.city,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+
+# review_seed = Review.new(
+#   content: Faker::Restaurant.review,
+#   rating: rand(0..5)
+#   )
+# review_seed.restaurant = restaurant_seed
+# review_seed.save
+end
+
+puts 'Finished!'
